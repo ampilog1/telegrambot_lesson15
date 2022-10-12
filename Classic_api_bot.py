7c22bce7e26720061b7fce9160a97a55598c967b
Dmitriy <ampilog1@gmail.com>
2022-10-12 12:48:01 +0300

    код с лекции

--- Classic_api_bot.py
+++ Classic_api_bot.py
@@ -1,7 +1,24 @@
 import requests
+import pprint
 
 TOKEN = '5787565526:AAEFoKR2CtyJqwFMUDmK9Cf2qvcMdILuEVg'
 MAIN_URL = f'https://api.telegram.org/bot{TOKEN}'
 url = f'{MAIN_URL}/getMe'
+# result = requests.get(url)
+# print(result.json())
+
+
+url = f'{MAIN_URL}/getUpdates'
+
 result = requests.get(url)
-print(result.json())
+# pprint.pprint(result.json())
+messages = result.json()['result']
+# pprint.pprint(messages)
+for message in messages:
+    chat_id = message['message']['chat']['id']
+    url = f'{MAIN_URL}/sendMessage'
+    params = {
+        'chat_id': chat_id,
+        'text': 'Добрый день, Дмитрий!'
+    }
+    result = requests.post(url, params=params)
