f606f790ce202dfccadf05d61e7545aa9b817844
Dmitriy <ampilog1@gmail.com>
2022-10-13 12:54:37 +0300

    код с лекции

--- lib_api02.py
+++ lib_api02.py
@@ -1,13 +1,19 @@
 import telebot
 
-TOKEN = '5787565526:AAEFoKR2CtyJqwFMUDmK9Cf2qvcMdILuEVg'
+TOKEN = '5629751285:AAG5C2UGTV4-U85lu1n5aihkLOp8P0ieU2I'
 
-bot = telebot.TeleBot("TOKEN", parse_mode=None)
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(content_types='text')
+def reverse_text(message):
+    text = message.text[::-1]
+    bot.reply_to(message, text)
 
 
 bot.infinity_polling()
