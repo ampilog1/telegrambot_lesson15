e2c9d4277ff17c7684402c16b3cd585679aa8d91
Dmitriy <ampilog1@gmail.com>
2022-10-17 12:55:40 +0300

    код с лекции

--- lib_api02.py
+++ lib_api02.py
@@ -1,3 +1,5 @@
+import time
+
 import telebot
 
 TOKEN = '5629751285:AAG5C2UGTV4-U85lu1n5aihkLOp8P0ieU2I'
@@ -10,10 +12,19 @@ def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
 
+@bot.message_handler(commands=['timer'])
+def timer(message):
+    for i in range(5):
+        time.sleep(1)
+        bot.reply_to(message, f'{i+1}')
+
+
+
 @bot.message_handler(content_types='text')
 def reverse_text(message):
     text = message.text[::-1]
     bot.reply_to(message, text)
+
 
 
 bot.infinity_polling()
